98a9c0be98946bb5ce82abe847a4733436bae208
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-02-05 15:32:40 +0000

    Changed [0..-2] back to .chomp method and removed hobbies, country-of-birth and height attributes.

--- directory.rb
+++ directory.rb
@@ -47,18 +47,15 @@ def print_students_list()
   puts "Which students do you want to look up? Use a - z."
   puts "If you want to look at all students, type all"
   while true do
-    choice = gets[0..-2]
+    choice = gets.chomp
     if choice == 'all'
       index = 0
       #code exercise to not use the each method and to create one using while loop
       #code exercise to only produce names under 12 characters long
       while index < @students.length
         if @students[index][:name].length < 12
-          puts ("#{index + 1}. #{@students[index][:name]} " +
-          "(#{@students[index][:cohort]} cohort). " +
-          "\n hobby: #{@students[index][:hobby]} " +
-          "\n country of birth: #{@students[index][:country_of_birth]} " +
-          "\n height: #{@students[index][:height]} tall.").center(20)
+          puts "#{index + 1}. #{@students[index][:name]} " +
+          "(#{@students[index][:cohort]} cohort). "
         end
         index += 1
       end
@@ -69,11 +66,8 @@ def print_students_list()
       #code exercise to only produce names under 12 characters long
       while index < @students.length
         if (@students[index][:name][0] == choice.downcase) && (@students[index][:name].length < 12)
-          puts ("#{index + 1}. #{@students[index][:name]} " +
-          "(#{@students[index][:cohort]} cohort). " +
-          "\n hobby: #{@students[index][:hobby]} " +
-          "\n country of birth: #{@students[index][:country_of_birth]} " +
-          "\n height: #{@students[index][:height]} tall.").center(20)
+          puts "#{index + 1}. #{@students[index][:name]} " +
+          "(#{@students[index][:cohort]} cohort). "
         end
         index += 1
       end
@@ -95,25 +89,15 @@ end
 def input_students
   puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
-  name = gets[0..-2]
+  name = gets.chomp
   while !name.empty? do
     puts "and the students cohort"
-    cohort = gets[0..-2].to_sym
+    cohort = gets.chomp.to_sym
     cohort = :february if cohort.empty?
-    #code exercise to add hobbies
-    puts "and that students hobby"
-    hobby = gets[0..-2]
-    #code exercise to add country of birth
-    puts "and that students country of birth"
-    country_of_birth = gets[0..-2]
-    #code exercise to add height
-    puts "and that students height"
-    height = gets[0..-2]
-    @students << {name: name, cohort: cohort, hobby: hobby, country_of_birth:
-      country_of_birth, height: height}
+    @students << {name: name, cohort: cohort}
     puts "Now we have #{@students.count} students"
     puts "And the next students name:"
-    name = gets[0..-2]
+    name = gets.chomp
   end
   @students.sort_by{|student| student[:cohort]}
 end
