2caf066291038da49f5b853c12bef00d7a2d8f71
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-02-06 16:13:23 +0000

    Added load_students method

--- directory.rb
+++ directory.rb
@@ -15,6 +15,17 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    # parallel assignment. As its an array, first value goes in first variable,
+    # and second value goes into second variable
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu()
@@ -30,6 +41,8 @@ def process(selection)
     show_students()
   when "3"
     save_students()
+  when "4"
+    load_students()
   when "9"
     exit # this will cause the program to terminate
   else
@@ -41,6 +54,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
